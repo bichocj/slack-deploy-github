dcef0419e940caa6e6f7258502bcc16fb28f86bc
bicho <bichocj@gmail.com>
2017-03-16 15:37:14 -0500

    update settings config

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,4 +1,10 @@
-API_TOKEN = "xoxb-155459157075-S2yaOUvxCttHweHfJPe2DLeH"
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# TODO get this token from slack api https://api.slack.com/apps
+API_TOKEN = config['BASE'].get('API_TOKEN')
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 

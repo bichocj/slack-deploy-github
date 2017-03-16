cf9ec59efbf1e9b140b21a88e3babd97f656991e
bicho <bichocj@gmail.com>
2017-03-16 17:26:48 -0500

    update bash_command

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,6 +1,23 @@
+echo
+echo "cd /srv/www/eventus/src/"
 cd /srv/www/eventus/src/
+
+echo
+echo "git pull"
 git pull
+
+echo
+echo ". /srv/venv/bin/activate"
 . /srv/venv/bin/activate
+
+echo
+echo "python manage.py migrate"
 python manage.py migrate
+
+echo
+echo "python manage.py collectstatic --no-input"
 python manage.py collectstatic --no-input
+
+echo
+echo "service apache2 restart"
 service apache2 restart

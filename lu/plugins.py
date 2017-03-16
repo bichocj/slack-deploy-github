22943f1df6145c5f93d854b720b0b8135e3d1471
bicho <bichocj@gmail.com>
2017-03-16 15:23:53 -0500

    update with commands and plugin

--- lu/plugins.py
+++ lu/plugins.py
@@ -1,27 +1,45 @@
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
 import re
+import subprocess
+
+from slackbot.bot import listen_to
+from slackbot.bot import respond_to
+
 
 @respond_to('hi', re.IGNORECASE)
 def hi(message):
-    message.reply('I can understand hi or HI!')
-    # react with thumb up emoji
+    message.reply('hi-me -> jaime XD')
     message.react('+1')
 
-@respond_to('I love you', re.IGNORECASE)
-def love(message):
-    message.reply('I love you too Dayi!')
 
-@listen_to('Can someone help me?', re.IGNORECASE)
+@respond_to('help', re.IGNORECASE)
 def help(message):
-    # Message is replied to the sender (prefixed with @user)
-    message.reply('Yes, I can!'
+    message.reply('I going to do: ')
+    with open('bash_commands', "r") as f:
+        for line in f:
+            message.reply("$ " + line)
 
-    # Message is sent on the channel
-    # message.send('I can help everybody!')
+    message.reply("write 'deploy' if you want I run the script")
+    message.reply("also you can give me your own command usin 'exec (.*)'")
 
 
-@listen_to('Who I am?', re.IGNORECASE)
-def help(message):
-    # Message is replied to the sender (prefixed with @user)
-    message.reply('An Atix Member super saya 3!')
+@respond_to('deploy', re.IGNORECASE)
+def deploy(message):
+    message.reply('ok!, I\'m beginning with de deploy')
+
+    with open('bash_commands', "r") as f:
+        for line in f:
+            p = subprocess.Popen(line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            text = ''
+            for line in p.stdout.readlines():
+                text += line.decode("utf-8")
+            message.reply(text)
+    f.close()
+
+
+@respond_to('exec (.*)')
+def exec_command(message, something):
+    p = subprocess.Popen(something, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    text = ''
+    for line in p.stdout.readlines():
+        text += line.decode("utf-8")
+    message.reply(text)
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,4 +1,4 @@
-API_TOKEN = ""
+API_TOKEN = "xoxb-155459157075-S2yaOUvxCttHweHfJPe2DLeH"
 
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 

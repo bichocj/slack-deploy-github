efa8861813ae79d28d92a72abaa7b2b00329acf5
bicho <bichocj@gmail.com>
2017-06-21 13:22:15 -0500

    update lu for go service -- undo

--- lu/plugins.py
+++ lu/plugins.py
@@ -44,14 +44,14 @@ def deploy(message):
         message.reply(e)
 
 
-@respond_to('up (.*)')
-def up_service(message, line):
-    try:
-        if line is 'go':
-            exec_order_and_reply_it(GO_SERVICE_COMMAND, message)
-            message.reply('_        $ service ' + line + ' is up')
-    except Exception as e:
-        message.reply(e)
+# @respond_to('up (.*)')
+# def up_service(message, line):
+#     try:
+#         if line is 'go':
+#             exec_order_and_reply_it(GO_SERVICE_COMMAND, message)
+#             message.reply('_            $ service ' + line + ' is up')
+#     except Exception as e:
+#         message.reply(e)
 
 
 @respond_to('exec (.*)')
